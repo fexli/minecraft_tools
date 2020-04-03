30c0093420eada09a860f46a6ad019d58401012f
fexli <62332061+fexli@users.noreply.github.com>
2020-04-03 12:15:42 +0800

    添加成书转换

--- warp2dimension.py
+++ warp2dimension.py
@@ -2,6 +2,7 @@ def asInt(hex_):
     return hex_[0] & 0xFF | (hex_[1] & 0xFF) << 8 | (hex_[2] & 0xFF) << 16 | (hex_[3] & 0xFF) << 24
 
 
+# 字符串转换为维度数据
 def warp2Dimension(str_):
     if isinstance(str_,str):
         import hashlib
@@ -14,6 +15,15 @@ def warp2Dimension(str_):
     else:
         print('Unsupported value encountered.')
         return None
+
+
+# warp2Dimension_fromBook(第一页内容字符串,第二页内容字符串...)
+# 每页中换行符用\n(而非\\n)表示
+# 单页、非换行的自写成书效果与/warp <str>指令效果相同
+# 可以通过NBTExplorer等软件查看非自己编写成书的内容
+def warp2Dimension_fromBook(*eachPage):
+    return warp2Dimension('\n'.join(eachPage))
+
 
 if __name__ == '__main__':
     import sys
